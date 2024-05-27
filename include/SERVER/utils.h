699f22521a86b31e4f3e929c0c451e6a40856331
Romain <romain.collignon@epitech.eu>
2024-05-27 14:45:06 +0200

    [+] add archi include for server

---
+++ include/SERVER/utils.h
@@ -0,0 +1,69 @@
+/*
+** EPITECH PROJECT, 2024
+** B4 - Network Programming
+** File description:
+** UTILS
+*/
+
+#ifndef UTILS_H_
+    #define UTILS_H_
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////  LIB  /////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+    #include <fcntl.h>
+    #include <stdlib.h>
+    #include <stdio.h>
+    #include <unistd.h>
+    #include <sys/stat.h>
+    #include <ncurses.h>
+    #include <signal.h>
+    #include <ctype.h>
+    #include <netinet/in.h>
+    #include <sys/socket.h>
+    #include <string.h>
+    #include <time.h>
+    #include <stdbool.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <stdbool.h>
+    #include <sys/select.h>
+    #include <arpa/inet.h>
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////  UTILS  ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////// MY_MALLOC and MY_FREE ///
+void *my_malloc(size_t size);
+void full_free(void);
+void my_free(void *ptr);
+void *my_realloc(void *ptr, size_t size);
+char *my_strdup(const char *str);
+    /////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////  COLOR  ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+    // Color:
+    #define CDEF  "\x1B[0m"
+
+    #define CBLD  "\x1B[1m"
+    #define CFNT  "\x1B[2m"
+    #define CITA  "\x1B[3m"
+    #define CUND  "\x1B[4m"
+    #define CINV  "\x1B[7m"
+    #define CTGH  "\x1B[9m"
+    #define CBLK  "\x1B[30m"
+    #define CRED  "\x1B[31m"
+    #define CGRN  "\x1B[32m"
+    #define CYEL  "\x1B[33m"
+    #define CBLU  "\x1B[34m"
+    #define CMAG  "\x1B[35m"
+    #define CCYN  "\x1B[36m"
+    #define CWHT  "\x1B[37m"
+
+#endif /* !UTILS_H_ */
