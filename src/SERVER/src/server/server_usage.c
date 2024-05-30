043d49299adb79e02f6a9e272bee53b3d9ae5169
Leprêtre <leo.lepretre@epitech.eu>
2024-05-30 16:52:59 +0200

    [+] gestion type of client (ai or gui)

--- src/SERVER/src/server/server_usage.c
+++ src/SERVER/src/server/server_usage.c
@@ -7,13 +7,14 @@
 
 #include "all.h"
 
-void server_usage(server_t *server)
+void server_usage(server_t *server, struct_t *s)
 {
     server->addrlen = sizeof(server->serv_adr);
+    server->round = 0;
     while (1) {
         server->tmp_fdtab = server->fd_tab;
         handle_activity(server);
         handle_new_client(server);
-        handling_cmd(server);
+        handling_cmd(server, s);
     }
 }
