66fe5e296f9d805ec9764aa4ca373700f18d2dab
Romain <romain.collignon@epitech.eu>
2024-06-02 00:26:45 +0200

    [+] server - add function ppo (GUI)

--- src/SERVER/src/commands_gui/c_ppo/ppo.c
+++ src/SERVER/src/commands_gui/c_ppo/ppo.c
@@ -7,9 +7,24 @@
 
 #include "all.h"
 
+static void parse_ppo(const char *input, int *n)
+{
+    int result;
+
+    *n = 0;
+    result = sscanf(input, "ppo #%d", n);
+    if (result < 1)
+        *n = 0;
+}
+
 int c_ppo(struct_t *s, char *buffer)
 {
-    (void)buffer;
-    printf("c_ppo\n");
+    int nb_player = 0;
+    player_t *player;
+
+    parse_ppo(buffer, &nb_player);
+    player = get_player_by_id(s, nb_player);
+    dprintf(s->fd_gui, "ppo #%d %d %d %d\n", nb_player, player->x, player->y,
+        player->view_direction);
     return 0;
 }
