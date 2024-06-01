19f586ac1695d033b8f220f6a9b5e49dc54e96d1
Romain <romain.collignon@epitech.eu>
2024-06-02 00:25:43 +0200

    [+] server - add function bct (GUI)

--- src/SERVER/src/commands_gui/c_bct/bct.c
+++ src/SERVER/src/commands_gui/c_bct/bct.c
@@ -7,9 +7,37 @@
 
 #include "all.h"
 
+void parse_bct(const char *input, int *x, int *y)
+{
+    int result;
+
+    *x = 0;
+    *y = 0;
+    result = sscanf(input, "bct %d %d", x, y);
+    if (result < 2) {
+        *x = 0;
+        *y = 0;
+    }
+}
+
 int c_bct(struct_t *s, char *buffer)
 {
-    (void)buffer;
-    printf("c_bct\n");
+    int x;
+    int y;
+    map_element_t *element;
+
+    parse_bct(buffer, &x, &y);
+    if (x < 0 || x >= s->map_width || y < 0 || y >= s->map_height)
+        return -1;
+    element = &s->map[y][x];
+    dprintf(s->fd_gui, "bct %d %d %d %d %d %d %d %d %d\n",
+        x, y,
+        element->food,
+        element->linemate,
+        element->deraumere,
+        element->sibur,
+        element->mendiane,
+        element->phiras,
+        element->thystame);
     return 0;
 }
