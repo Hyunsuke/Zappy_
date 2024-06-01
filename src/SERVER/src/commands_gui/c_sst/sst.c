73d97e5ebb7458d8e36e19a1ba1ca278fd84c418
Romain <romain.collignon@epitech.eu>
2024-06-02 00:27:17 +0200

    [+] server - add function sst (GUI)

--- src/SERVER/src/commands_gui/c_sst/sst.c
+++ src/SERVER/src/commands_gui/c_sst/sst.c
@@ -7,9 +7,26 @@
 
 #include "all.h"
 
+static void parse_sst(const char *input, int *n)
+{
+    int result;
+
+    *n = 0;
+    result = sscanf(input, "pin #%d", n);
+    if (result < 1)
+        *n = -1;
+}
+
 int c_sst(struct_t *s, char *buffer)
 {
-    (void)buffer;
-    printf("c_sst\n");
+    int unit_time;
+
+    parse_sst(buffer, &unit_time);
+    if (unit_time < 0) {
+        return -1;
+    } else {
+        s->time = unit_time;
+        dprintf(s->fd_gui, "sst %d\n", s->time);
+    }
     return 0;
 }
