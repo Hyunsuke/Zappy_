25cde648cfa981bda246e4297d806a259d5e6a83
Romain <romain.collignon@epitech.eu>
2024-06-02 00:26:06 +0200

    [+] server - add function pin (GUI)

--- src/SERVER/src/commands_gui/c_pin/pin.c
+++ src/SERVER/src/commands_gui/c_pin/pin.c
@@ -7,9 +7,33 @@
 
 #include "all.h"
 
+static void parse_pin(const char *input, int *n)
+{
+    int result;
+
+    *n = 0;
+    result = sscanf(input, "pin #%d", n);
+    if (result < 1)
+        *n = 0;
+}
+
 int c_pin(struct_t *s, char *buffer)
 {
-    (void)buffer;
-    printf("c_pin\n");
+    int nb_player;
+    player_t *player;
+
+    parse_pin(buffer, &nb_player);
+    player = get_player_by_id(s, nb_player);
+    dprintf(s->fd_gui, "pin #%d %d %d %d %d %d %d %d %d %d\n",
+        nb_player,
+        player->x,
+        player->y,
+        player->food,
+        player->linemate,
+        player->deraumere,
+        player->sibur,
+        player->mendiane,
+        player->phiras,
+        player->thystame);
     return 0;
 }
