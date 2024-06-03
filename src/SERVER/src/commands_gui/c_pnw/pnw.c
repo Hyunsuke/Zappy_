46a1367249f8fa86ca31efca1f1ee53afad61d43
Romain <romain.collignon@epitech.eu>
2024-06-03 14:56:58 +0200

    [+] server - add function c_pnw (GUI)

--- src/SERVER/src/commands_gui/c_pnw/pnw.c
+++ src/SERVER/src/commands_gui/c_pnw/pnw.c
@@ -7,8 +7,14 @@
 
 #include "all.h"
 
-int c_pnw(struct_t *s)
+// Position x;y;
+int c_pnw(struct_t *s, int id_player, int *position, int incantation_level)
 {
-    printf("c_pnw\n");
+    dprintf(s->fd_gui, "pwn #%d %d %d %d %d %d\n",
+        id_player,
+        position[0], position[1],
+        get_player_by_id(s, id_player)->view_direction,
+        incantation_level,
+        get_team_by_id(s, get_player_by_id(s, id_player)->id_team));
     return 0;
 }
