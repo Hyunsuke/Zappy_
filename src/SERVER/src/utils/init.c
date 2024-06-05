23b0861b2601392d97d81d46eb1f20ef377553f1
Romain <romain.collignon@epitech.eu>
2024-06-05 15:35:56 +0200

    [+] server - add function for init struct

---
+++ src/SERVER/src/utils/init.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** init.c
+** File description:
+** Function
+*/
+
+#include "all.h"
+
+void init_struct(struct_t *s)
+{
+    s->head_player = NULL;
+    s->next_id_player = 0;
+    s->next_id_team = 0;
+    s->fd_gui = -1;
+    generator_map(s);
+}
