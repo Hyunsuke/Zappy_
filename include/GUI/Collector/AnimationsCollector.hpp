6e34a6510f7bbdaa7104d53b96e7e9e253d35a7f
Maxence <maxence.poirette@epitech.eu>
2024-06-10 18:22:46 +0200

    [+] adding the animation collector

---
+++ include/GUI/Collector/AnimationsCollector.hpp
@@ -0,0 +1,29 @@
+// AnimationCollector.hpp
+
+#ifndef ANIMATIONCOLLECTOR_HPP
+#define ANIMATIONCOLLECTOR_HPP
+
+#include <string>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+#include "raylib.h"
+
+class AnimationCollector {
+public:
+    static AnimationCollector& GetInstance();
+
+    std::shared_ptr<ModelAnimation> LoadAnimation(const std::string& filePath, int* animCount);
+    void UnloadAllAnimations();
+
+private:
+    AnimationCollector() = default;
+    ~AnimationCollector();
+    AnimationCollector(const AnimationCollector&) = delete;
+    AnimationCollector& operator=(const AnimationCollector&) = delete;
+
+    std::unordered_map<std::string, std::shared_ptr<ModelAnimation>> animationCache;
+    std::unordered_map<std::string, int> animCountCache;
+};
+
+#endif // ANIMATIONCOLLECTOR_HPP
