d21df667f78cb69d264b05ae3442f8548c884144
Romain <romain.collignon@epitech.eu>
2024-06-20 14:45:00 +0200

    [+] SERVER - add input for change time unit in DASHBOARD

--- src/SERVER/src/commands_gui/c_sst/sst.c
+++ src/SERVER/src/commands_gui/c_sst/sst.c
@@ -12,7 +12,7 @@ static void parse_sst(const char *input, int *n)
     int result;
 
     *n = 0;
-    result = sscanf(input, "pin %d", n);
+    result = sscanf(input, "sst %d", n);
     if (result < 1)
         *n = -1;
 }
@@ -27,6 +27,31 @@ int c_sst(struct_t *s, char *buffer)
     } else {
         s->time = unit_time;
         dprintf(s->fd_gui, "sst %d\n", s->time);
+    }
+    return 0;
+}
+
+static void parse_sst_dashboard(const char *input, int *n)
+{
+    int result;
+
+    *n = 0;
+    result = sscanf(input, "sst %d\n", n);
+    if (result < 1)
+        *n = -1;
+}
+
+int c_sst_dashboard(struct_t *s, char *buffer)
+{
+    int unit_time;
+
+    parse_sst(buffer, &unit_time);
+    if (unit_time < 0) {
+        return -1;
+    } else {
+        s->time = unit_time;
+        if (s->fd_gui != -1)
+            dprintf(s->fd_gui, "sst %d\n", s->time);
     }
     return 0;
 }
